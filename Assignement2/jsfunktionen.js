127a4db550f826233e279b67f7c4f1ec10870e62
Sladjana Spasenic <sladjana.spasenic@stud.uni-bamberg.de>
2016-05-31 19:35:08 +0200

    jsfunktionen.js : GET-Request eingebaut (funktioniert nicht, da noch ids und weitere Infos fehlen)

--- Assignement2/jsfunktionen.js
+++ Assignement2/jsfunktionen.js
@@ -1,5 +1,23 @@
 
 $(document).ready(function(){
     /* Hier der jQuery-Code */
-   
-});
+	
+	<!-- /* Hier irgendwo müssen per GET-Request (Kombi aus jQuery und AJAX)  
+die .php-Dateien zu den Modulen eingebunden werden */ -->
+
+/* Hier die dazugehörigen ids im Body bauen, die aufgerufen werden (einzelne ids= einzelne Donut-Abschnitte*/
+
+<script type="text/javascript">
+		$(function() {
+			$('#submit').click(function(){
+				var MyText = $('input[id=MeinText]').val();	 // id anpassen
+				
+				$.get("moduleGroups.php",
+					  { text: MyText },									// anpassen
+				      function(data) {
+					     $('#result').html(data);						// was ist in dem Fall result? das Laden der entsprecheneden Infos zum Modul
+                         alert('Load was performed.');
+                      });
+			});
+		});
+	</script>
