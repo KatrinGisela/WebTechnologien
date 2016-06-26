bcefc8c0b254fdab0bf750980eb3626cf7725c55
Sladjana Spasenic <sladjana.spasenic@stud.uni-bamberg.de>
2016-06-26 23:04:08 +0200

    Model für Partie erstellt

---
+++ Assignement3/Fußballturnier/app/models/Partie.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.HashMap;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.avaje.ebean.Model;
+
+import play.data.validation.Constraints.Required;
+
+public class Partie extends Model   {
+  
+ 	
+ 	@Id
+ 	public Long PartieID;
+ 	@Required
+ 	public String nameHeimat; 
+  @Required	  
+  public String nameGast   ; 
+  @Required  
+  public String datum;
+  @OneToOne(mappedBy = "parties")
+  // gibts OneToOne??
+ 	public List<Stadium> stadiums;    // überprüfen, ob Namen matchen (zudem grammatikalisch inkorrekt)
+  
+ 	public static Finder<Long, Partie> find = new Finder<Long, Partie>(Partie.class);
+  
+ 	public static void create(Partie partie) {
+    	// if (mindestens ein Stadion existiert in der Datenbank)
+  		partie.save();
+ 	}
+  
+ 	public static List<Partie> read() {
+  		return find.all();
+  	}
+  
+ 	public static void update(Partie updatedParties) {
+  		updatedPartie.update();
+ 	}
+  
+ 	public static void delete(Long PartieID) {
+    find.ref(PartieID).delete();
+ 	}
+  
+ 	public static Map<Long, String> getAllAsMap() {
+  		HashMap<Long, String> partiesMap = new HashMap<Long, String>();
+  		List<Partie> parties = Partie.read();
+  		for (Partie partie : parties) {
+      		
+   			partiesMap.put(partie.partieID, partie.nameHeimat, partie.nameGast, partie.datum );
+  		}
+    		return partiesMap;
+ 	}
+}
+  }
