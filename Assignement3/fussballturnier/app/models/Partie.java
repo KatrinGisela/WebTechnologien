f83b5acb8b64dce8ea672bc4a0837aa309ac8ed6
Andreas Wolter <andreas.wolter@stud.uni-bamberg.de>
2016-06-30 11:23:45 +0200

    bennene die Klassen um und passe die Variablen- und Methodennamen in Partie an

---
+++ Assignement3/fussballturnier/app/models/Partie.java
@@ -0,0 +1,49 @@
+package models;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+public class Partie extends Model {
+	@Id
+	public Long iid;
+	@Required
+	public String name;
+	@Required
+	public String heimmannschaftName;
+	@Required
+	public String gastmannschaftName;
+	@Required
+	public String partieDatum;
+	@ManyToMany(mappedBy = "partien")
+	public List<Stadion> stadien;
+
+	public static Finder<Long, Partie> find = new Finder<Long, Partie>(
+			Partie.class);
+
+	public static void create(Partie partie) {
+		partie.save();
+	}
+
+	public static List<Partie> read() {
+		return find.all();
+	}
+
+	public static void update(Partie updatedPartie) {
+		updatedPartie.update();
+	}
+
+	public static void delete(Long pid) {
+		find.ref(pid).delete();
+	}
+
+	public static Map<Long, String> getAllAsMap() {
+		HashMap<Long, String> partienMap = new HashMap<Long, String>();
+		List<Partie> partien = Partie.read();
+		for (Partie partie : partien) {
+			partienMap.put(partie.iid, partie.name);
+		}
+		return partienMap;
+	}
+}
