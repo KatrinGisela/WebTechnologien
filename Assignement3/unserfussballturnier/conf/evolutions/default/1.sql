b83c18c5aaf418f6de2d48404dbcc604388529b8
Andreas Wolter <andreas.wolter@stud.uni-bamberg.de>
2016-07-10 12:00:00 +0200

    aktualisiere die Datenbank

--- Assignement3/unserfussballturnier/conf/evolutions/default/1.sql
+++ Assignement3/unserfussballturnier/conf/evolutions/default/1.sql
@@ -3,50 +3,57 @@
 
 # --- !Ups
 
-create table ingredient (
-  iid                       bigint not null,
+create table partie (
+  pid                       bigint not null,
   name                      varchar(255),
+  heimmannschaft_name       varchar(255),
+  gastmannschaft_name       varchar(255),
+  partie_datum              varchar(255),
   description               varchar(255),
-  constraint pk_ingredient primary key (iid))
+  constraint pk_partie primary key (pid))
 ;
 
-create table recipe (
-  rid                       bigint not null,
+create table stadion (
+  sid                       bigint not null,
   name                      varchar(255),
+  addresse                  varchar(255),
+  anzahl_sitzplaetze        integer,
+  anzahl_zugaenge           integer,
+  sponsor                   varchar(255),
   description               varchar(255),
-  constraint pk_recipe primary key (rid))
+  constraint pk_stadion primary key (sid))
 ;
 
 
-create table recipe_ingredient (
-  recipe_rid                     bigint not null,
-  ingredient_iid                 bigint not null,
-  constraint pk_recipe_ingredient primary key (recipe_rid, ingredient_iid))
+create table stadion_partie (
+  stadion_sid                    bigint not null,
+  partie_pid                     bigint not null,
+  constraint pk_stadion_partie primary key (stadion_sid, partie_pid))
 ;
-create sequence ingredient_seq;
+create sequence partie_seq;
 
-create sequence recipe_seq;
-
-
+create sequence stadion_seq;
 
 
-alter table recipe_ingredient add constraint fk_recipe_ingredient_recipe_01 foreign key (recipe_rid) references recipe (rid) on delete restrict on update restrict;
 
-alter table recipe_ingredient add constraint fk_recipe_ingredient_ingredie_02 foreign key (ingredient_iid) references ingredient (iid) on delete restrict on update restrict;
+
+alter table stadion_partie add constraint fk_stadion_partie_stadion_01 foreign key (stadion_sid) references stadion (sid) on delete restrict on update restrict;
+
+alter table stadion_partie add constraint fk_stadion_partie_partie_02 foreign key (partie_pid) references partie (pid) on delete restrict on update restrict;
 
 # --- !Downs
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists ingredient;
+drop table if exists partie;
 
-drop table if exists recipe_ingredient;
+drop table if exists stadion;
 
-drop table if exists recipe;
+drop table if exists stadion_partie;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
-drop sequence if exists ingredient_seq;
+drop sequence if exists partie_seq;
 
-drop sequence if exists recipe_seq;
+drop sequence if exists stadion_seq;
 
